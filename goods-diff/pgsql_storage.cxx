80e4f673cf3c80cbf771f7d3910cca6b84d0db20
Konstantin Knizhnik <knizhnik@garret.ru>
2016-05-08 23:12:06 +0300

    pgsql_storage added

--- goods-diff/pgsql_storage.cxx
+++ goods-diff/pgsql_storage.cxx
@@ -1,71 +1,141 @@
-using namespace pqxx;
+#inlude "pgsql_storage.h"
 
-#define MAX_TABLES 16
-#define GET_TID(x) ((x) % MAX_TABLES)
+#define MAX_TABLES 256
+#define GET_CID(x) ((x) % MAX_TABLES)
 #define GET_OID(x) ((x) / MAX_TABLES)
-#define CONS_OPID(t,o) ((o)*MAX_TABLES + (t))
+#define MAKE_OPID(t,o) ((o)*MAX_TABLES + (t))
 #define OID_BIAS 0x10000
 
-void pgsql_storage::pgsql_storage(char const* connStr) : con(connStr), tableMap(class_descriptor::last_ctid)
+inline string get_table(class_descriptor* desc)
 {
-	con.prepare("alloc", "select nextval('oid_sequence') from generate_series(1,$1"); 
-	con.prepare("get_class", "select desc from classes where oid=?"); 
-	con.prepare("put_class", "insert into classes (desc) values ($1)"); 
-	con.prepare("change_class", "update classes set desc=$1 where oid=$2"); 
+	while (desc->base_class != &object::self_class) { 
+		desc = desc->base_class;
+	}
+	return string(desc->name);
+}
 
+static void get_columns(string prefix, field_descriptor* first, vector<string>& columns)
+{
+	field_descriptor* field = first;
+	
+    do { 
+		if (field->field->loc.type == fld_structure) { 
+			get_columns(prefix + field->name, field->components, columns);
+		} else { 
+			columns->push_back(prefix + field->name);
+		}
+        field = (field_descriptor*)field->next;
+    } while (field != first);
+}
+
+static string get_host(string address)
+{
+	return address.substr(0, address.find(':'));
+}
+
+static string get_port(string address)
+{
+	return address.substr(address.find(':')+1);
+}
+
+void pgsql_storage::open(char const* connection_address, const char* login, const char* password) 
+{
+	opid_buf_pos = 0;
+	con = new connection(string("user=") + login + " password=" + password + " host=" + get_host(connection_address) + " port=" + get_port(connection_address));
+	con->prepare("alloc", "select nextval('oid_sequence') from generate_series(1,$1"); 
+	con->prepare("get_class", "select desc from classes where oid=?"); 
+	con->prepare("put_class", "insert into classes (desc) values ($1)"); 
+	con->prepare("change_class", "update classes set desc=$1 where oid=$2"); 
 	for (size_t i = 0; i < DESCRIPTOR_HASH_TABLE_SIZE; i++) { 
-		for (class_descriptor* cls = class_descriptor::hash_table[i]; cls != NULL; cls = cls->next) { 
-			if (cls->base_class == &object::self_class) { 
-				tableMap[cls->ctid] = tables.size();
-				tables.push_back(cls->name);
-				con.prepare(tables.back() + "_delete", string("delete from ") + tables.back() + " where oid=$1"); 
-			} else { 
-				class_descriptor* base;
-				for (base = cls->base_class; base->base_class == &object::self_class; base = base->base_class);
-				tableMap[cls->ctid] = tableMap[base->ctid];
+		for (class_descriptor* cls = class_descriptor::hash_table[i]; cls != NULL; cls = cls->next) {
+			vector<string> columns;
+			string table_name = get_table(cls);
+			string class_name(cls->name);
+			get_columns("", cls->fields, columns);
+			{
+				std::stringstream sql;			
+				sql << "insert into " << table_name << " (";
+				for (size_t i = 0; i < columns.size(); i++) { 
+					if (i != 0) sql << ',';
+					sql << columns[i];
+				}
+				sql << ") values (";
+				for (size_t i = 0; i < columns.size(); i++) { 
+					if (i != 0) sql << ',';
+					sql << '$' << (i+1);
+				}
+				sql << ")";
+				con->prepare(class_name + "_insert", sql);
+			}
+			{
+				std::stringstream sql;			
+				sql << "update " << class_name << " set ";
+				for (size_t i = 0; i < columns.size(); i++) { 
+					if (i != 0) sql << ',';
+					sql << columns[i] << "=$" << (i+2);
+				}
+				sql << " where opid=$1";
+				con->prepare(class_name + "_update", sql);
+			}
+			if (table_name == class_name) {
+				con->prepare(table_name + "_delete", string("delete from ") + table_name + " where opid=$1");
+				con->prepare(table_name + "_loadobj", string("select * from ") + table_name + " where opid=$1");
+				con->prepare(table_name + "_loadset", string("with recursive set_members(opid,obj) as (select m.opid,m.obj from set_member m where m.prev=$1 union all select m.opid,m.obj from set_member m join set_members s ON m.prev=s.opid) select s.opid as mbr_opid,s.next as mbr_next,s.prev as mbr_prev,s.owner as mbr_owner,s.obj as mbr_obj,s.key as mbr_key,t.* from set_members s, ") + table_name + " t where t.opid=s.obj limit $2");
+
 			}
 		}
 	}
 }
 
-void pgsql_storage::bulk_allocate(size_t sizeBuf[], cpid_t cpidBuf[], size_t nAllocObjects, 
-                                       opid_t opidBuf[], size_t nReservedOids, hnd_t clusterWith[])
+void pgsql_storage::close()
 {
-	result rs = txn->prepared("alloc")(nReservedOids).exec();
-	for (size_t i = 0; i < nReservedOids; i++) { 
-		opidBuf[i] = rs[i][0].as(opid_t());
-	}
+	delete con;
+}
+
+
+
+void pgsql_storage::bulk_allocate(size_t sizeBuf[], cpid_t cpidBuf[], size_t nAllocObjects, 
+                                       opid_t opid_buf[], size_t nReservedOids, hnd_t clusterWith[])
+{
+	// Bulk allocate at obj_storage level is disabled 
+	assert(false);
 }
 
 opid_t pgsql_storage::allocate(cpid_t cpid, size_t size, int flags, opid_t clusterWith)
 {
-	opid_t opid;
-	bulk_allocate(NULL, NULL, 0, &opid, 1, NULL);
-	return opid;
+	if (opid_buf_pos == OPID_BUF_SIZE) { 
+		result rs = txn->prepared("alloc")(OPID_BUF_SIZE).exec();
+		for (size_t i = 0; i < OPID_BUF_SIZE; i++) { 
+			opid_buf[i] = rs[i][0].as(opid_t());
+		}
+		opid_buf_pos = 0;
+	}
+	return MAKE_OPID(opid_buf{opid_buf_pos++], cpid);
 }
 
-void dbs_client_storage::deallocate(opid_t opid)
+void pgsql_storage::deallocate(opid_t opid)
 { 
-	txn->prepared(tables[tablesMap[GET_TID(opid)]] + "_delete")(GET_OID(opid)).exec();
+	class_descriptor* desc = lookup_class(GET_CID(opid));
+	txn->prepared(get_table(desc) + "_delete")(opid).exec();
 }
 
-boolean dbs_client_storage::lock(opid_t opid, lck_t lck, int attr)
+boolean pgsql_storage::lock(opid_t opid, lck_t lck, int attr)
 {
 	return true;
 }
 
-void dbs_client_storage::unlock(opid_t opid, lck_t lck)
+void pgsql_storage::unlock(opid_t opid, lck_t lck)
 {
 }
 
-void dbs_client_storage::get_class(cpid_t cpid, dnm_buffer& buf)
+void pgsql_storage::get_class(cpid_t cpid, dnm_buffer& buf)
 {
 	result rs = txt->prepared("get_class")(cpid + OID_BIAS).exec();
 	binarystring desc = rs[0][0].as(binarystring());
 	memcpy(buf.put(desc.size()), desc.data());
 }
 
-cpid_t dbs_client_storage::put_class(dbs_class_descriptor* dbs_desc)
+cpid_t pgsql_storage::put_class(dbs_class_descriptor* dbs_desc)
 {
 	size_t dbs_desc_size = dbs_desc->get_size();
 	binarystring buf(dbs_desc, dbs_desc_size);
@@ -74,7 +144,7 @@ cpid_t dbs_client_storage::put_class(dbs_class_descriptor* dbs_desc)
 	return rs.oid() - OID_BIAS;
 }
 
-void dbs_client_storage::change_class(cpid_t cpid, 
+void pgsql_storage::change_class(cpid_t cpid, 
                                       dbs_class_descriptor* dbs_desc)
 {
 	size_t dbs_desc_size = dbs_desc->get_size();
@@ -84,6 +154,438 @@ void dbs_client_storage::change_class(cpid_t cpid,
 }
 	
 
-void dbs_client_storage::query(opid_t& next_mbr, char const* query, nat4 buf_size, int flags, nat4 max_members, dnm_buffer& buf)
+void pgsql_storage::load(opid_t opid, int flags, dnm_buffer& buf)
+{
+	load(&opid, 1, flags, buf);
+}
+
+class_descriptor* pgsql_storage::lookup_class(cpid_t cpid)
+{
+	class_descriptor* desc = cpid < descriptor_table.size() ? descriptor_table[cpid] : NULL;
+	if (desc == NULL) { 
+		dnm_buffer cls_buf;
+		get_class(cpid, cls_buf);
+		dbs_class_descriptor* dbs_desc = (dbs_class_descriptor*)&cls_buf;
+		dbs_desc->unpack();
+		desc = class_descriptor::find(dbs_desc->name());
+		assert(desc != NULL);
+		assert(desc->dbs_desc != *dbs_desc);
+		if (descriptor_table.size() <= cpid) { 
+			descriptor_table.resize(cpid);
+		}
+		descriptor_table[cpid] = desc;
+	}
+	return desc;
+}
+
+static size_t unpack_struct(field_descriptor* first, string prefix, 
+							dnm_buffer& buf, tuple const& record, size_t n_refs)
+{
+	field_descriptor* field = first;
+    do { 
+		tuple::reference col = record[prefix + field->name];
+		assert(!col.is_null() || field->loc.type == fld_string);
+		if (field->loc.n_items != 1) { 
+			assert(field->loc.type == fld_signed_integer && field->loc.size == 1);
+			string str = col.as(string());
+			if (varying_length == 0) { 
+				char* dst = buf.append(str.size());
+				memcpy(dst, str.data(), str.size());
+			} else { 
+				char* dst = buf.append(field->loc.n_items);
+				if (field->loc.n_items <= str.size) { 
+					memcpy(dst, str.data(), field->loc.n_items);
+				} else { 
+					memcpy(dst, str.data(), str.size());
+					memset(dst + str.size(), 0, field->loc.n_items - str.size());
+				}
+			}
+		} else {
+			switch(field->loc.type) { 
+			  case fld_reference:
+			  {
+				  char* dst = (dbs_referece_t*)&buf + n_refs++;
+				  nat8 objref = col.as(nat8());
+				  opid_t opid = (opid_t)objref;
+				  packref(dst, (stid_t)(objref >> 32), opid);
+				  break;			  
+			  }
+			  case fld_string:
+			  {
+				  if (col.is_null()) { 
+					  char* dst = buf.append(2);
+					  pack2(dst, 0xFFFF);
+				  } else {
+					  string str = col.as(string());
+					  std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+					  std::wstring wstr = conv.from_bytes(str);
+					  char* dst = buf.append((1 + str.size())*2);
+					  dst = pack2(dst, str.size());
+					  for (size_t i = 0i i < str.size(); i++) { 
+						  dst = pack2(dst, str[i]);
+					  }
+				  }
+				  break;
+			  }
+			  case fld_raw_binary:
+			  {
+				  binarystring blob = col.as(binarystring());
+				  char* dst = buf.append(4 + blob.size());
+				  dst = pack4(dst, blob.size());
+				  memcpy(dst, blob.data(), blob.size());
+				  break;
+			  }
+			  case fld_signed_integer:
+			  case fld_unsigned_integer:
+				switch (field->loc.size) { 
+				  case 1:				
+					*buf.append(1) = col.as(int1());
+					break;
+				  case 2:
+					pack2(buf.append(2), col.as(int2()));
+					break;
+				  case 4:
+					pack4(buf.append(4), col.as(int4()));
+					break;
+				  case 8:
+					pack8(buf.append(8), col.as(int8()));
+					break;
+				  default:
+					assert(false);
+				}
+				break;
+			  case fld_real:
+				switch (field->loc.size) { 
+				  case 4:
+				  {
+					  union {
+						  float f;
+						  int4  i;
+					  } u;
+					  u.f = col.as(float());
+					  pack4(buf.append(4), u.i); 
+					  break;
+				  }
+				  case 8:
+				  {
+					  union {
+						  double f;
+						  int8   i;
+					  } u;
+					  u.f = col.as(double());
+					  pack8(buf.append(8), u.i); 
+					  break;
+				  }
+				  default:
+					assert(false);
+				}
+				break;
+			  case fld_structure:
+				n_regs = load_struct(string(field->name) + ".", field->coponents, buf, record, n_refs);
+				break;
+			  default:
+				asset(false);
+			}
+		}
+        field = (field_descriptor*)field->next;
+    } while (field != first);
+
+	return n_refs;
+}
+
+static opid_t load_query_result(result& rs, dnm_buffer& buf);
+
+static void unpack_object(string prefix, class_descriptor* desc, dnm_buffer& buf, tuple const& record)
+{
+	dbs_object_header* hdr = (dbs_object_header*)buf.append(sizeof(dbs_object_header) + desc->n_fixed_references*sizeof(dbs_reference_t)); 
+	size_t hdr_offs = buf.size();
+	opid_t opid = record[prefix + "opid"].as(opid_t());
+	hdr->set_opid(opid);		
+	hdr->set_cpid(GET_CID(opid));
+	hdr->set_sid(sid);
+	hdr->set_flags(0);
+                
+	size_t n_refs = unpack_struct(desc->fields, prefix, buf, record, 0);
+	assert(n_refs == desc->n_fixed_references);
+	hdr = (dbs_object_header*)(&buf + hdr_offs);
+	hdr->size = buf.size() - hdr_offs;
+
+	if (desc == &set_member::self_class && prefix.size() == 0) { 
+		opid_t mbr_obj_opid;
+		stid_t mbr_obj_sid;
+		// unpack referene of set_member::obj
+		unpackref(mbr_obj_sid, mbr_obj_opid, &buf + hdr_offs + sizeof(dbs_object_header) + sizeof(dbs_reference_t)*3);
+		cpid_t cpid = GET_CID(mbr_obj_opid);
+		string table_name = get_table(lookup_class(cpid));
+		result rs = txn->prepared(table_name  + "loadset")(opid)(max_preloaded_set_members).exec();
+		load_query_result(rs, buf);
+	}
+	return opid;
+}
+
+static opid_t load_query_result(result& rs, dnm_buffer& buf)
+{
+	opid_t next_mbr = 0;
+	for (auto i = rs.begin(); i != rs.end(); ++i) {
+		tuple obj_records = *i;
+		opid_t obj_opid = obj_record["opid"].as(opid_t());
+		class_descriptor* obj_desc = lookup_class(GET_CID(obj_opid));
+		size_t mbr_buf_offs = buf.size();
+		unpack_object("mbr_", &set_member::self_class, buf, obj_records);
+		stid_t next_sid;
+		unpackref(next_sid, next_mbr, &buf + mbr_buf_offs + sizeof(dbs_object_header));
+		unpack_object("", obj_desc, buf, obj_records); 
+	}
+	return next_mbr;
+}
+
+
+
+void pgsql_storage::query(opid_t& next_mbr, char const* query, nat4 buf_size, int flags, nat4 max_members, dnm_buffer& buf)
+{
+	load(next_mbr, flags, buf);
+	stid_t sid;
+	opid_t opid;
+	unpackref(sid, opid, &buf + sizeof(dbs_object_header) + 3*sizeof(dbs_referece_t));
+	cpid_t cpid = GET_CID(opid);
+	class_descriptor* desc = lookup_class(cpid);
+	string table_name = get_table(desc);
+	std::stringstream sql;
+	sql << "with recursive set_members(opid,obj) as (select m.opid,m.obj from set_member m where m.oipd=" << opid << " union all select m.opid,m.obj from set_member m join set_members s ON m.prev=s.opid) select s.opid as mbr_opid,s.next as mbr_next,s.prev as mbr_prev,s.owner as mbr_owner,s.obj as mbr_obj,s.key as mbr_key,t.* from set_members s, " << table_name << " t where t.opid=s.obj and " << query << " limit " << max_members;
+	result rs = txn->exec(sql.str());
+	buf.cut(buf.size());
+	next_mbr = load_query_result(rs, buf);
+}
+
+
+void pgsql_storage::load(opid_t* opp, int n_objects, 
+						 int flags, dnm_buffer& buf)
+{
+	for (int i = 1; i < n_objects; i++) { 
+		opid_t opid = opp[i];
+		cpid_t cpid = GET_CID(opid);
+		class_descriptor* desc = lookup_class(cpid);		
+		result rs = txn->prepared(get_table(desc) + "_loadobj")(GET_OID(opid)).exec();
+		unpack_object("", desc, buf, rs[0]);
+	}
+}
+
+void pgsql_storage::forget_object(opid_t opid)
 {
 }
+
+
+void pgsql_storage::throw_object(opid_t opid)
+{
+}
+
+void pgsql_storage::begin_transaction(dnm_buffer& buf)
+{
+	txn = new work(*con);
+}
+
+
+static size_t store_struct(field_descriptor* first, prepare::invocation& stmt, char* &src_refs, char* &src_bins, size_t size)
+{
+	field_descriptor* field = first;
+	
+    do { 
+		if (field->loc.n_items != 1) { 
+			assert(field->loc.type == fld_signed_integer && field->loc.size == 1);
+			string str = col.as(string());
+			if (field->loc.n_items == 0) {
+				stmt(string(src_bins, size));
+				src_bins += size;
+				size = 0;
+			} else { 
+				stmt(string(src_bins, field->loc.n_items));
+				src_bins += field->loc.n_items;
+				size -= field->loc.n_items;
+			}
+		} else {
+			switch(field->loc.type) { 
+			  case fld_reference:
+			  {
+				  opid_t opid;
+				  stid_t sid;
+				  src_refs = unpackref(sid, opid, src_refs);
+				  size -= sizeof(dbs_reference_t);
+				  stmt(opid);
+				  break;			  
+			  }
+			  case fld_string:
+			  {
+				  size_t len = unpack2(src_bins);
+				  src_bins += 2;
+				  size -= 2;
+				  if (len != 0xFFFF) { 
+					  vectpor<wchar_t> buf(len);
+					  for (size_t i = 0; i < len; i++) { 
+						  buf[i] = unpack2(src_bins);
+						  src_bins += 2;
+						  size -= 2;
+					  }
+					  std::wstring wstr(&buf, len);
+					  std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+					  std::string str = conv.to_bytes(wstr);
+					  stmt(str);
+				  } else {
+					  stmt(); // store NULL
+				  }
+				  break;
+			  }
+			  case fld_raw_binary:
+			  {
+				  size_t len = unpack4(src_bins);
+				  binarystring blob(src_bins+4, len);
+				  stmt(blob);
+				  src_bins += 4 + len;
+				  size -= 4 + len;
+				  break;
+			  }
+			  case fld_signed_integer:
+				switch (field->loc.size) { 
+				  case 1:	
+					stmt(*src_bins++);
+					size -= 1;
+					break;
+				  case 2:
+					stmt(unpack2(src_bins));
+					src_bins += 2;
+					size -= 2;
+					break;
+				  case 4:
+					stmt(unpack4(src_bins));
+					src_bins += 4;
+					size -= 4;
+					break;
+				  case 8:
+					stmt(unpack8(src_bins));
+					src_bins += 8;
+					size -= 8;
+					break;
+				  default:
+					assert(false);
+				}
+				break;
+			  case fld_unsigned_integer:
+				switch (field->loc.size) { 
+				  case 1:	
+					stmt((nat1)*src_bins++);
+					size -= 1;
+					break;
+				  case 2:
+					stmt((nat2)unpack2(src_bins));
+					src_bins += 2;
+					size -= 2;
+					break;
+				  case 4:
+					stmt((nat4)unpack4(src_bins));
+					src_bins += 4;
+					size -= 4;
+					break;
+				  case 8:
+					stmt((nat8)unpack8(src_bins));
+					src_bins += 8;
+					size -= 8;
+					break;
+				  default:
+					assert(false);
+				}
+				break;
+			  case fld_real:
+				switch (field->loc.size) { 
+				  case 4:
+				  {					  
+					  union {
+						  float f;
+						  int4  i;
+					  } u;
+					  u.i = unpack4(src_bins);
+					  stmt(u.f);
+					  src_bins += 4;
+					  size -= 4;
+					  break;
+				  }
+				  case 8:
+				  {
+					  union {
+						  double f;
+						  int8   i;
+					  } u;
+					  u.i = unpack8(src_bins);
+					  stmt(u.f);
+					  src_bins += 8;
+					  size -= 8;
+					  break;
+				  }
+				  default:
+					assert(false);
+				}
+				break;
+			  case fld_structure:
+				size = store_struct(field->coponents, stmt, src_bins, src_refs, size);
+				break;
+			  default:
+				asset(false);
+			}
+		}
+        field = (field_descriptor*)field->next;
+    } while (field != first);
+	
+	return size;
+}	
+
+boolean pgsql_storage::commit_coordinator_transaction(int n_trans_servers,
+													  stid_t*trans_servers,
+													  dnm_buffer& buf, 
+													  trid_t& tid)
+{
+	assert(n_trans_servers == 1);
+	char* ptr = &buf;
+	char* end = ptr + buf.size();
+	while (ptr < end) { 
+		dbs_object_header* hdr = (dbs_object_header*)&buf;
+		int flags = hdr->get_flags();
+		class_descriptor* desc = lookup_class(hdr->get_cpid());
+		prepare::invocation stmt = txn->preapred(string(desc->name) + ((flags & tof_update) ? "_update" : "_insert"));
+		stmt(hdr->get_opid());
+		size_t left = store_struct(desc->fields, stmt, (char*)(hdr+1), hdr->get_size());
+		assert(left == 0);
+		stmt.exec();
+	}
+	txn->commit();
+	delete txn;
+}
+	
+void pgsql_storage::commit_transaction(stid_t      coordinator, 
+                                            int         n_trans_servers,
+                                            stid_t*     trans_servers,
+                                            dnm_buffer& buf, 
+                                            trid_t      tid)
+{
+	assert(false);
+}
+
+boolean pgsql_storage::wait_global_transaction_completion()
+{
+	return false;
+}
+
+void pgsql_storage::send_messages() {}
+	
+nat8 pgsql_storage::get_used_size()
+{
+	work txn(*con);
+	result rs = txn.exec("select pg_database_size(current_database())");	
+	return rs[0][0].as(nat8_t());
+}
+
+boolean pgsql_storage::start_gc()
+{
+	return false;
+}
+
+void pgsql_storage::add_user(char const* login, char const* password){}
+
+void pgsql_storage::del_user(char const* login){}
---
+++ goods-diff/pgsql_storage.h
@@ -0,0 +1,160 @@
+// -*- mode: c++; tab-width: 8; c-basic-offset: 4 -*-
+//-< STORAGE.H >-----------------------------------------------------*--------*
+// GOODS                     Version 1.0         (c) 1997  GARRET    *     ?  *
+// (Generic Object Oriented Database System)                         *   /\|  *
+//                                                                   *  /  \  *
+//                          Created:      7-Jan-97    K.A. Knizhnik  * / [] \ *
+//                          Last update: 23-Apr-97    K.A. Knizhnik  * GARRET *
+//-------------------------------------------------------------------*--------*
+// Abstract database storage interface.  
+//-------------------------------------------------------------------*--------*
+
+#ifndef __PGSQL_STORAGE_H__
+#define __PGSQL_STORAGE_H__
+
+#include "goodsdlx.h"
+#include "protocol.h"
+
+#include <pqxx/connection>
+#include <pqxx/transaction>
+#include <pqxx/prepared_statement>
+#include <pqxx/except>
+#include <pqxx/binarystring>
+
+using namespace pqxx;
+using namespace pqxx::prepare;
+
+BEGIN_GOODS_NAMESPACE
+
+#define OPID_BUF_SIZE 64
+
+//
+// This class provides bridge to PostgreSQL
+//
+class GOODS_DLL_EXPORT pgsql_storage : public dbs_storage { 
+	work* txn;
+	connection* con;
+	opid_t opid_buf[OPID_BUF_SIZE];
+	size_t opid_buf_pos;
+
+  public:
+	pgsql_storage(stid_t sid) : dbs_storage(sid, NULL) {}
+
+    virtual opid_t  allocate(cpid_t cpid, size_t size, int flags, opid_t clusterWith);
+    virtual void    bulk_allocate(size_t sizeBuf[], cpid_t cpidBuf[], size_t nAllocObjects, 
+                                  opid_t opidBuf[], size_t nReservedOids, hnd_t clusterWith[]);
+    virtual void    deallocate(opid_t opid);
+
+    virtual boolean lock(opid_t opid, lck_t lck, int attr);
+    virtual void    unlock(opid_t opid, lck_t lck);
+    
+    //
+    // Get class descriptor by class identifier.
+    // Class descriptor is placed in the buffer supplied by application.
+    // If there is no such class at server buf.size() is set to 0.
+    //
+    virtual void    get_class(cpid_t cpid, dnm_buffer& buf);
+    //
+    // Store class descriptor at server receive it's identifier
+    //
+    virtual cpid_t  put_class(dbs_class_descriptor* desc);
+    //
+    // Change existed class descriptor
+    //
+    virtual void    change_class(cpid_t cpid, dbs_class_descriptor* desc);
+    //                                        
+    // Send a message to others clients. 
+    //
+    virtual void    send_message( int message);
+    //
+    // Schedule a user message to others clients. 
+    //
+    virtual void    push_message( int message);
+    //
+    // Send all scheduleds user messages to others clients. 
+    //
+    virtual void    send_messages();
+                                            
+    //
+    // Load object from server into the buffer supplied by application.
+    // Before each object dbs_object_header structure is placed.
+    // If there is no such object at server then "cpid" field of 
+    // dbs_object_header is set to 0.
+    //
+    virtual void    load(opid_t* opid, int n_objects, 
+                         int flags, dnm_buffer& buf);
+
+    virtual void    load(opid_t opid, int flags, dnm_buffer& buf);
+
+
+    virtual void    query(opid_t& next_mbr, char const* query, nat4 buf_size, int flags, nat4 max_members, dnm_buffer& buf);
+
+    //
+    // Inform server that client no more has reference to specified object
+    // 
+    virtual void    forget_object(opid_t opid);
+    //
+    // Inform server that client no more has instance of specified object
+    //
+    virtual void    throw_object(opid_t opid); 
+
+    //
+    // Initiate transaction at server. Allocate place for transaction header
+    // in buffer. All objects involved in transaction should be appended 
+    // to this buffer. 
+    //
+    virtual void    begin_transaction(dnm_buffer& buf); 
+
+    //
+    // Commit local transaction or part of global transaction 
+    // at coordinator server. In latter case coordinator will return 
+    // transaction indentifier which should be 
+    // passed to all other servers participated in transaction.
+    // If transaction is aborted by server "False" is returned. 
+    //
+    virtual boolean commit_coordinator_transaction(int n_trans_servers, 
+                                                   stid_t* trans_servers,
+                                                   dnm_buffer& buf, 
+                                                   trid_t& tid);
+    //
+    // Commit local part of global transaction at server. 
+    //
+    virtual void    commit_transaction(stid_t coordinator, 
+                                       int n_trans_servers,
+                                       stid_t* trans_servers,
+                                       dnm_buffer& buf, 
+                                       trid_t tid);
+    //
+    // Wait completion of global transaction (request to coordinator)
+    // If transaction is aborted by server "False" is returned. 
+    //
+    virtual boolean wait_global_transaction_completion();
+
+    virtual boolean open(const char* server_connection_name, char const* login = NULL, char const* password = NULL);
+    virtual void    close();
+
+    virtual nat8    get_used_size();
+
+    virtual boolean start_gc();
+
+    virtual void    add_user(char const* login, char const* password);
+    virtual void    del_user(char const* login);
+};
+
+
+class GOODS_DLL_EXPORT pgsql_index : public b_tree
+{
+    virtual ref<set_member> find(const char* str, size_t len, skey_t key) const;
+    virtual ref<set_member> find(const char* str) const;
+    virtual ref<set_member> find(skey_t key) const;
+    virtual ref<set_member> findGE(skey_t key) const;
+    virtual ref<set_member> findGE(const char* str, size_t len, skey_t key) const;
+    virtual ref<set_member> findGE(const char* str) const;
+
+    virtual void insert(ref<set_member> mbr);
+    virtual void remove(ref<set_member> mbr);
+	virtual void clear();
+};
+	
+
+#endif
